35b76d9769e6eacb4e10d1df2cc666826104749a
gondesiasha <50900238+gondesiasha@users.noreply.github.com>
2019-05-29 20:59:31 -0700

    Add files via upload

---
+++ routes/books1.js
@@ -0,0 +1,84 @@
+var express=require('express')
+var router=express.Router();
+var mysql=require('mysql');
+
+var books=[
+{
+	id:1,
+	title:"JS fundamentals",
+	author:"john"
+
+},
+{
+	id:2,
+	title:"node fundamentals",
+	author:"David"
+
+},
+{
+	id:3,
+	title:"angular fundamentals",
+	author:"asha"
+
+}
+]
+router.route("/")
+.get((req,res)=>{
+	 var connection = mysql.createConnection({
+            host     : "localhost",
+            user     : "root",
+            password : "",
+            database : "asha"
+          });
+          connection.connect();
+ 
+          connection.query('SELECT * from Books', function (error, books, fields) {
+            if (error) throw error;
+            res.send(books)
+          });
+           
+          connection.end();
+
+
+	//res.send(books)
+})
+.post((req,res)=>{
+	var newBook=req.body;
+
+        var connection = mysql.createConnection({
+            host     : "localhost",
+            user     : "root",
+            password : "",
+            database : "asha"
+          });
+          connection.connect();
+ 
+          connection.query(`INSERT INTO Books(title,author_fname,author_lname) VALUES('${newBook.title}','${newBook.author_fname}','${newBook.author_lname}')`, 
+                            function (error, book, fields) {
+            if (error) throw error;
+            res.send(book)
+          });
+           
+          connection.end();
+    })
+
+
+router.route("/:id")
+.put((req,res)=>
+{
+	var id=req.params.id;
+	var bookToBeupdated=books.filter((book)=>{
+		return book.id==id
+	})
+	bookToBeupdated[0].title="oracale pro"
+	res.send(bookToBeupdated)
+})
+.delete((req,res)=>{
+	var id=req.params.id;
+	var latestBooks=books.filter((book)=>{
+		return book.id!=id
+	})
+	res.send(latestBooks)
+
+})
+module.exports=router;
